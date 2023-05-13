800266c25a7037674dedc115f51ed5321cbc0080
Bob Weigel <rweigel@gmu.edu>
2023-05-13 13:10:19 -0400

    move files

--- runs/Middelpos_read.m
+++ runs/Middelpos_read.m
@@ -3,45 +3,50 @@
 % Dir of this script
 scriptpath = fileparts(mfilename('fullpath')); 
 
-% Dir of .t82 files
-datapath = [scriptpath,'/data/Middelpos']; 
+name = 'Dealesville';
+ext = 't06';
+inpath = [scriptpath,'data/Dealesville/measurements'];
+outpath = [scriptpath,'data/Dealesville'];
 
-datapath = [scriptpath,'/data/Middelpos/measurements']; 
-dlist = dir(datapath);
-
-% Create .mat version of data in .t82 files
-for i = 1:length(dlist)
-    fname = [dlist(i).folder,'/',dlist(i).name];
-    if endsWith(dlist(i).name,'t82') && ~exist(fname,'file')
-        fprintf('Reading: %s\n',fname);
-        data = load(fname);
-        save([fname,'.mat'],'data');
-        fprintf('Wrote:   %s\n',[fname,'.mat']);                
-    end
+if 1
+name = 'Middelpos';
+ext = 't82';
+inpath = [scriptpath,'/data/Middelpos/measurements']; 
+outpath = [scriptpath,'/data/Middelpos']; 
 end
 
-% Read and concatenate .mat files
+dlist = dir(inpath);
+
+% Create .mat version of data in all text files
 Data = [];
 first = '';
 for i = 1:length(dlist)
-    if endsWith(dlist(i).name,'mat')
-        last = dlist(i).name(1:end-8);
-        if isempty(first)
-          first = dlist(i).name(1:end-8);
-        end
-        fname = [dlist(i).folder,'/',dlist(i).name];
-        fprintf('Reading: %s\n',fname);                
-        data = load(fname);
-        Data = [Data; data.data];
+    fname = [dlist(i).folder,'/',dlist(i).name];
+    fnamemat = [dlist(i).folder,'/',dlist(i).name,'.mat'];
+    if ~endsWith(dlist(i).name,ext)
+        continue;
     end
+    try
+        fprintf('Reading: %s\n',fname);
+        data = load(fname);
+    catch ex
+        fprintf('Failed read. Skipping.\n');
+        ex
+    end
+    fprintf('Writing: %s\n',fnamemat);
+    save(fnamemat,'data');
+    last = dlist(i).name(1:end-8);
+    if isempty(first)
+      first = dlist(i).name(1:end-8);
+    end
+    Data = [Data; data];
 end
 
 B = Data(:,7:9);
 E = Data(:,12:13);
 t = datenum(Data(:,1:6));
 
-datapath = [scriptpath,'/data/Middelpos/']; 
-fname = sprintf('Middelpos_%s-%s_raw.mat',first,last);
-matfile = [datapath,'/',fname]; 
+fname = sprintf('%s_%s-%s_raw.mat',name,first,last);
+matfile = [outpath,'/',fname]; 
 save(matfile,'B','E','t');
 fprintf('Wrote: %s\n',matfile);
